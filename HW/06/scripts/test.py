3a89c84c645ad4a641e555bc8944378acafd32de
Vasiliy <alvasian@yandex.ru>
2018-07-26 00:39:25 +0300

    add test psycopg script

---
+++ HW/06/scripts/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import psycopg2
+
+try:
+	conn = psycopg2.connect(
+		host = 'localhost',
+		database = 'shop_data',
+		user = 'postgres',
+		password = 'postgres'
+	)
+except:
+	print 'Failed'
+finally:
+	if conn is not None:
+		conn.close()
+		print 'Connection closed'
